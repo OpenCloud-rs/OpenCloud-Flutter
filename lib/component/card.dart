736ff30d676b55cee9a80db0d09242dfe69149b1
Rheydskey <matgag02@gmail.com>
2020-07-30 12:20:21 +0200

    Improvement: Add an animation to the card when pressed

--- lib/component/card.dart
+++ lib/component/card.dart
@@ -9,29 +9,29 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Container(
         padding: new EdgeInsets.only(bottom: 3.0),
-        child: new GestureDetector(
-            onTap: func,
-            child: new Card(
-              child: new Container(
-                  padding: new EdgeInsets.all(35),
-                  child: new Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
+        child: new Card(
+            child: new InkWell(
+          onTap: func,
+          child: new Container(
+              padding: new EdgeInsets.all(35),
+              child: new Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  new Column(
                     children: <Widget>[
-                      new Column(
-                        children: <Widget>[
-                          this.title,
-                        ],
-                      ),
-                      new Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          this.but,
-                        ],
-                      )
+                      this.title,
                     ],
-                  )),
-            )));
+                  ),
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      this.but,
+                    ],
+                  )
+                ],
+              )),
+        )));
   }
 }
